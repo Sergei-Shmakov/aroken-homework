79573947e65af21f89982ce0d1afed8dc1f011cf
Sergei-Shmakov <153823538+Sergei-Shmakov@users.noreply.github.com>
2025-07-23 10:05:54 +0700

    diplom added

---
+++ dyson/js/main.js
@@ -0,0 +1,15 @@
+;(function () {
+	//burger
+	document.addEventListener('click', burgerInit)
+	function burgerInit(e) {
+		const target = e.target
+		const burgerIcon = target.closest('.burger-icon')
+		const burgerNavLink = target.closest('.nav__link')
+		if (!burgerIcon && !burgerNavLink) return
+		if (!document.body.classList.contains('body--opened-menu')) {
+			document.body.classList.add('body--opened-menu')
+		} else {
+			document.body.classList.remove('body--opened-menu')
+		}
+	}
+})()
